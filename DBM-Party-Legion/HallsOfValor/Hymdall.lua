4483e0507c363844251d2b3ece11e3f799095d1c
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2024-01-23 01:55:23 +0400

    fix

--- DBM-Party-Legion/HallsOfValor/Hymdall.lua
+++ DBM-Party-Legion/HallsOfValor/Hymdall.lua
@@ -16,33 +16,28 @@ mod:RegisterEventsInCombat(
 	"SPELL_PERIODIC_MISSED 193234"
 )
 
---[[
-(ability.id = 191284 or ability.id = 193235 or ability.id = 193092) and type = "begincast"
- or ability.id = 188404 and type = "cast"
- or type = "dungeonencounterstart" or type = "dungeonencounterend"
---]]
 local warnBreath					= mod:NewCountAnnounce(188404, 4)
-local warnDancingBlade				= mod:NewTargetAnnounce(193235, 3)
-local warnSweep						= mod:NewSpellAnnounce(193092, 2, nil, "Tank")
+local warnDancingBlade				= mod:NewTargetAnnounce(193235, 3) --Танцующий клинок
 
-local specWarnHornOfValor			= mod:NewSpecialWarningDefensive(191284, nil, nil, nil, 3, 2)
+local specWarnSweep					= mod:NewSpecialWarningDefensive(193092, "Tank", nil, nil, 3, 2) --Рог доблести
+local specWarnHornOfValor			= mod:NewSpecialWarningDefensive(191284, "-Tank", nil, nil, 3, 2) --Рог доблести
 local specWarnDancingBlade			= mod:NewSpecialWarningMove(193235, nil, nil, nil, 1, 8) --Танцующий клинок
-local specWarnDancingBlade2			= mod:NewSpecialWarningYou(193235, nil, nil, nil, 3, 6) --Танцующий клинок
---local yellDancingBlade				= mod:NewYell(193235)
+local specWarnDancingBlade2			= mod:NewSpecialWarningYou(193235, nil, nil, nil, 3, 2) --Танцующий клинок
 
 local timerSweepCD					= mod:NewCDTimer(16.9, 193092, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
-local timerDancingBladeCD			= mod:NewCDTimer(10, 193235, nil, nil, nil, 3)
-local timerHornCD					= mod:NewCDTimer(43.8, 191284, nil, nil, nil, 2)
-local timerBreathCast				= mod:NewCastCountTimer(43.8, 188404, nil, nil, nil, 3)
+local timerDancingBladeCD			= mod:NewCDTimer(10, 193235, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)
+local timerHornCD					= mod:NewCDTimer(42.6, 191284, nil, nil, nil, 2)
+local timerBreathCast				= mod:NewCastCountTimer(43.8, 188404, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON)
 
-local yellDancingBlade				= mod:NewYell(193235, nil, nil, nil, "YELL")
+local yellDancingBlade				= mod:NewShortYell(193235, nil, nil, nil, "YELL") --Танцующий клинок
 
 mod:AddSetIconOption("SetIconOnDancingBlade", 193235, true, 0, {8})
 
 mod.vb.bladeCount = 0
 mod.vb.breathCount = 0
 
-function mod:DancingBladeTarget(targetname) --Танцующий клинок [✔] прошляпанного очка Мурчаля Прошляпенко
+--function mod:DancingBladeTarget(targetname, uId)
+function mod:DancingBladeTarget(targetname, uId) --Танцующий клинок [✔] прошляпанного очка Мурчаля Прошляпенко
 	if not targetname then return end
 	if targetname == UnitName("player") then
 		specWarnDancingBlade2:Show()
@@ -58,9 +53,9 @@ end
 
 function mod:OnCombatStart(delay)
 	self.vb.bladeCount = 0
-	timerDancingBladeCD:Start(5.2-delay)
-	timerHornCD:Start(10.8-delay)
-	timerSweepCD:Start(15.7-delay)
+	timerDancingBladeCD:Start(4.3-delay)
+	timerHornCD:Start(9.2-delay)
+	timerSweepCD:Start(15.3-delay)
 end
 
 function mod:SPELL_CAST_START(args)
@@ -69,17 +64,17 @@ function mod:SPELL_CAST_START(args)
 		self.vb.breathCount = 0
 		specWarnHornOfValor:Show()
 		specWarnHornOfValor:Play("defensive")
-		timerBreathCast:Start(8, 1)
+		timerBreathCast:Start(4.7, 1)
 		timerHornCD:Start()
 	elseif spellId == 193235 then
 		self.vb.bladeCount = self.vb.bladeCount + 1
-	--	warnDancingBlade:Show(self.vb.bladeCount)
-		self:ScheduleMethod(0.1, "BossTargetScanner", args.sourceGUID, "DancingBladeTarget", 0.1, 8)
 		if self.vb.bladeCount % 2 == 0 then
 			timerDancingBladeCD:Start(11.2)
 		else
 			timerDancingBladeCD:Start(31.1)
 		end
+	--	self:BossTargetScanner(args.sourceGUID, "DancingBladeTarget", 0.1, 2)
+		self:ScheduleMethod(0.1, "BossTargetScanner", args.sourceGUID, "DancingBladeTarget", 0.1, 8)
 	elseif spellId == 188404 then
 		self.vb.breathCount = self.vb.breathCount + 1
 		warnBreath:Show(self.vb.breathCount)
@@ -87,7 +82,8 @@ function mod:SPELL_CAST_START(args)
 			timerBreathCast:Start(5, self.vb.breathCount+1)
 		end
 	elseif spellId == 193092 then
-		warnSweep:Show()
+		specWarnSweep:Show()
+		specWarnSweep:Play("defensive")
 		timerSweepCD:Start()
 	end
 end
