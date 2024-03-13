710cb1176e8d8381bb50967acd901ee845a0c442
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2024-03-14 01:52:18 +0400

    fix

--- DBM-Party-WoD/SMBurialGrounds/Nhallish.lua
+++ DBM-Party-WoD/SMBurialGrounds/Nhallish.lua
@@ -11,7 +11,7 @@ mod.sendMainBossGUID = true
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 152801 153067",
+	"SPELL_CAST_START 152801 153067 152792",
 	"SPELL_AURA_APPLIED 152979",
 	"SPELL_AURA_REMOVED 152979",
 	"SPELL_PERIODIC_DAMAGE 153070",
@@ -23,15 +23,28 @@ mod:RegisterEventsInCombat(
  or ability.id = 152979 and type = "applydebuff"
  or type = "dungeonencounterstart" or type = "dungeonencounterend"
 --]]
+
+local specWarnVoidBlast			= mod:NewSpecialWarningDefensive(152792, nil, nil, nil, 3, 4) --Вспышка Бездны
 local specWarnVoidVortex		= mod:NewSpecialWarningRun(152801, nil, nil, 2, 4, 2)
 local specWarnSoulShred			= mod:NewSpecialWarningSpell(152979, nil, nil, nil, 1, 2)
-local specWarnVoidDevastation	= mod:NewSpecialWarningSpell(153067, nil, nil, nil, 2, 2)
+local specWarnVoidDevastation	= mod:NewSpecialWarningDodge(153067, nil, nil, nil, 2, 2)
 local specWarnVoidDevastationM	= mod:NewSpecialWarningGTFO(153070, nil, nil, nil, 1, 8)
 
 local timerVoidVortexCD			= mod:NewCDTimer(77, 152801, nil, nil, nil, 2)
 local timerSoulShredCD			= mod:NewNextTimer(77, 152979, nil, nil, nil, 6)
 local timerSoulShred			= mod:NewBuffFadesTimer(20, 152979)
 local timerVoidDevastationCD	= mod:NewNextTimer(77, 153067, nil, nil, nil, 3)
+
+local yellVoidBlast				= mod:NewShortYell(152792, nil, nil, nil, "YELL") --Вспышка Бездны
+
+function mod:VoidBlastTarget(targetname, uId)
+	if not targetname then return end
+	if targetname == UnitName("player") then
+		specWarnVoidBlast:Show()
+		specWarnVoidBlast:Play("defensive")
+		yellVoidBlast:Yell()
+	end
+end
 
 function mod:OnCombatStart(delay)
 	timerVoidVortexCD:Start(22.7-delay)
@@ -45,10 +58,12 @@ function mod:SPELL_CAST_START(args)
 		timerVoidVortexCD:Start()
 		specWarnVoidVortex:Show()
 		specWarnVoidVortex:Play("runaway")
-	elseif spellId == 153067 then
+	elseif spellId == 153067 then --Опустошение Бездны
 		specWarnVoidDevastation:Show()
-		specWarnVoidDevastation:Play("aesoon")
+		specWarnVoidDevastation:Play("watchstep")
 		timerVoidDevastationCD:Start()
+	elseif spellId == 152792 then --Вспышка Бездны
+		self:BossTargetScanner(args.sourceGUID, "VoidBlastTarget", 0.1, 2)
 	end
 end
 
