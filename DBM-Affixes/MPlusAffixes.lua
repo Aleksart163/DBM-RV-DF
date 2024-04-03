13e2e4bba52b03fde14c337debbe805356f97995
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2024-04-04 01:58:57 +0400

    up

--- DBM-Affixes/MPlusAffixes.lua
+++ DBM-Affixes/MPlusAffixes.lua
@@ -387,7 +387,7 @@ end
 local mod	= DBM:NewMod("MPlusAffixes", "DBM-Affixes")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20240305165926")
+mod:SetRevision("20240329111205")
 --mod:SetModelID(47785)
 mod:SetZone(DBM_DISABLE_ZONE_DETECTION)--Stays active in all zones for zone change handlers, but registers events based on dungeon ids
 
@@ -401,10 +401,10 @@ mod:RegisterEvents(
 
 --TODO, fine tune tank stacks/throttle?
 
-(ability.id = 240446 or ability.id = 409492) and type = "begincast"
- or (ability.id = 408556 or ability.id = 408801) and type = "applydebuff"
- or type = "dungeonencounterstart" or type = "dungeonencounterend"
- or (source.type = "NPC" and source.firstSeen = timestamp) and (source.name = "Afflicted Soul") or (target.type = "NPC" and target.firstSeen = timestamp) and (target.name = "Afflicted Soul")
+--(ability.id = 240446 or ability.id = 409492) and type = "begincast"
+--or (ability.id = 408556 or ability.id = 408801) and type = "applydebuff"
+-- or type = "dungeonencounterstart" or type = "dungeonencounterend"
+-- or (source.type = "NPC" and source.firstSeen = timestamp) and (source.name = "Afflicted Soul") or (target.type = "NPC" and target.firstSeen = timestamp) and (target.name = "Afflicted Soul")
 
 local warnExplosion							= mod:NewCastAnnounce(240446, 4)
 local warnIncorporeal						= mod:NewCastAnnounce(408801, 4)
@@ -508,8 +508,8 @@ do
 		--657, 1841, 1754, 1458, 2527, 2519, 2451, 2520
 		validZones = {[657]=true, [1841]=true, [1754]=true, [1458]=true, [2527]=true, [2519]=true, [2451]=true, [2520]=true}
 	elseif (C_MythicPlus.GetCurrentSeason() or 0) == 12 then--DF Season 4
-		--NOT YET KNOWN, season 3 placeholders
-		validZones = {[2579]=true, [1279]=true, [1501]=true, [1466]=true, [1763]=true, [643]=true, [1862]=true}
+		--2516, 2526, 2515, 2521, 2527, 2519, 2451, 2520
+		validZones = {[2516]=true, [2526]=true, [2515]=true, [2521]=true, [2527]=true, [2519]=true, [2451]=true, [2520]=true}
 	else--Season 3 (11) (latest LIVE season put in else so if api fails, it just always returns latest)
 		--2579, 1279, 1501, 1466, 1763, 643, 1862
 		validZones = {[2579]=true, [1279]=true, [1501]=true, [1466]=true, [1763]=true, [643]=true, [1862]=true}
@@ -588,6 +588,16 @@ function mod:SPELL_CAST_START(args)
 	end
 end
 
+
+--function mod:SPELL_CAST_SUCCESS(args)
+--	if not self.Options.Enabled then return end
+--	local spellId = args.spellId
+--	if spellId == 373370 then
+--		timerNightmareCloudCD:Start(30.5, args.sourceGUID)
+--	end
+--end
+
+
 function mod:SPELL_AURA_APPLIED(args)
 	if not self.Options.Enabled then return end
 	local spellId = args.spellId
@@ -650,13 +660,14 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
---function mod:SPELL_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
---	if spellId == 209862 and destGUID == UnitGUID("player") and self:AntiSpam(3, "aff7") then
---		specWarnGTFO:Show(spellName)
---		specWarnGTFO:Play("watchfeet")
---	end
---end
---mod.SPELL_MISSED = mod.SPELL_DAMAGE
+
+function mod:SPELL_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spellName)
+	if spellId == 209862 and destGUID == UnitGUID("player") and self:AntiSpam(3, "aff7") then
+		specWarnGTFO:Show(spellName)
+		specWarnGTFO:Play("watchfeet")
+	end
+end
+mod.SPELL_MISSED = mod.SPELL_DAMAGE
 --]]
 
 --<610.64 01:20:34> [CHAT_MSG_MONSTER_YELL] Marked by lightning!#Raszageth###Global Affix Stalker##0#0##0#3611#nil#0#false#false#false#false", -- [3882]
