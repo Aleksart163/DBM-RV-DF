7b4107543ad6d6c43ff9f2c635a1653ca5d1d175
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2024-04-18 03:19:47 +0400

    up and fix

--- DBM-Party-MoP/JadeSerpentTemple/LiuFlameheart.lua
+++ DBM-Party-MoP/JadeSerpentTemple/LiuFlameheart.lua
@@ -17,7 +17,8 @@ mod:RegisterEventsInCombat(
 	"SPELL_DAMAGE 107110",
 	"SPELL_MISSED 107110",
 	"SPELL_PERIODIC_DAMAGE 118540",
-	"SPELL_PERIODIC_MISSED 118540"
+	"SPELL_PERIODIC_MISSED 118540",
+	"UNIT_SPELLCAST_SUCCEEDED boss1 boss2"
 --	"UNIT_DIED"
 )
 
@@ -26,46 +27,66 @@ mod:RegisterEventsInCombat(
  or ability.id = 106797
  or type = "dungeonencounterstart" or type = "dungeonencounterend"
 --]]
-local warnDragonStrike			= mod:NewSpellAnnounce(106823, 2)
-local warnPhase2				= mod:NewPhaseAnnounce(2)
-local warnJadeDragonStrike		= mod:NewSpellAnnounce(106841, 3)
-local warnPhase3				= mod:NewPhaseAnnounce(3)
+local warnPhase					= mod:NewPhaseChangeAnnounce(2, 2, nil, nil, nil, nil, nil, 2)
+local warnDragonStrike			= mod:NewSpellAnnounce(106823, 2) --Разящий удар змеи
+local warnJadeDragonStrike		= mod:NewSpellAnnounce(106841, 4) --Разящий удар Нефритовой Змеи
 
-local specWarnDragonStrike		= mod:NewSpecialWarningDefensive(106823, nil, nil, nil, 1, 2)
-local specWarnDragonKick		= mod:NewSpecialWarningDodge(106856, nil, nil, nil, 2, 2)
-local specWarnJadeDragonStrike	= mod:NewSpecialWarningDefensive(106841, nil, nil, nil, 1, 2)
-local specWarnJadeDragonKick	= mod:NewSpecialWarningDodge(106864, nil, nil, nil, 2, 2)
-local specWarnJadeBreath		= mod:NewSpecialWarningDodge(396907, nil, nil, nil, 2, 2)
-local specWarnGTFO				= mod:NewSpecialWarningGTFO(118540, nil, nil, nil, 1, 8)
+local specWarnDragonStrike		= mod:NewSpecialWarningDefensive(106823, nil, nil, nil, 1, 4) --Разящий удар змеи
+local specWarnDragonKick		= mod:NewSpecialWarningDodge(106856, nil, nil, nil, 2, 2) --Удар змеи
+local specWarnJadeDragonStrike	= mod:NewSpecialWarningDefensive(106841, nil, nil, nil, 3, 4) --Разящий удар Нефритовой Змеи
+local specWarnJadeDragonKick	= mod:NewSpecialWarningDodge(106864, nil, nil, nil, 2, 2) --Повергающий удар Нефритовой Змеи
+local specWarnJadeBreath		= mod:NewSpecialWarningDodge(396907, nil, nil, nil, 2, 2) --Огненное нефритовое дыхание
+local specWarnJadeFire			= mod:NewSpecialWarningDodge(107045, nil, nil, nil, 2, 2) --Нефритовый огонь
+local specWarnGTFO				= mod:NewSpecialWarningGTFO(118540, nil, nil, nil, 1, 8) --Волна Нефритовой Змеи
 
-local timerDragonStrikeCD		= mod:NewNextTimer(15.7, 106823, nil, nil, 2, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.HEALER_ICON)--Kicks affect entire group as well (which are part of tank combo)
-local timerJadeDragonStrikeCD	= mod:NewNextTimer(15.7, 106841, nil, nil, 2, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.HEALER_ICON)--Kicks affect entire group as well (which are part of tank combo)
+local timerJadeFireCD			= mod:NewCDTimer(12, 107045, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON) --Нефритовый огонь
+local timerJadeBreathCD			= mod:NewCDTimer(10, 396907, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON) --Огненное нефритовое дыхание
+local timerDragonStrikeCD		= mod:NewNextTimer(15.7, 106823, nil, nil, 2, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.HEALER_ICON) --Разящий удар змеи Kicks affect entire group as well (which are part of tank combo)
+local timerJadeDragonStrikeCD	= mod:NewNextTimer(15.7, 106841, nil, nil, 2, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.HEALER_ICON) --Разящий удар Нефритовой Змеи Kicks affect entire group as well (which are part of tank combo)
 
 function mod:OnCombatStart(delay)
-	timerDragonStrikeCD:Start(11.6-delay)
+	timerDragonStrikeCD:Start(8-delay)
 end
 
- function mod:SPELL_CAST_START(args)
+function mod:OnCombatEnd()
+	self:UnregisterShortTermEvents()
+end
+
+function mod:DragonStrikeTarget(targetname, uId)
+	if not targetname then return end
+	if targetname == UnitName("player") then
+		specWarnDragonStrike:Show()
+		specWarnDragonStrike:Play("defensive")
+	else
+		warnDragonStrike:Show()
+	end
+end
+
+function mod:JadeDragonStrikeTarget(targetname, uId)
+	if not targetname then return end
+	if targetname == UnitName("player") then
+		specWarnJadeDragonStrike:Show()
+		specWarnJadeDragonStrike:Play("defensive")
+	else
+		warnJadeDragonStrike:Show()
+	end
+end
+
+function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 106797 then--Jade Essence (Phase 2 trigger)
-		warnPhase2:Show()
+		warnPhase:Show(DBM_CORE_L.AUTO_ANNOUNCE_TEXTS.stage:format(2))
+		warnPhase:Play("ptwo")
 		timerDragonStrikeCD:Cancel()
 		timerJadeDragonStrikeCD:Start(2.7)
+		self:RegisterShortTermEvents(
+			"INSTANCE_ENCOUNTER_ENGAGE_UNIT"
+		)
 	elseif args.spellId == 106823 then--Phase 1 dragonstrike
-		if self:IsTanking("player", "boss1", nil, true) then
-			specWarnDragonStrike:Show()
-			specWarnDragonStrike:Play("defensive")
-		else
-			warnDragonStrike:Show()
-		end
+		self:BossTargetScanner(args.sourceGUID, "DragonStrikeTarget", 0.1, 2)
 		timerDragonStrikeCD:Start()
 	elseif spellId == 106841 then--phase 2 dragonstrike
-		if self:IsTanking("player", "boss1", nil, true) then
-			specWarnJadeDragonStrike:Show()
-			specWarnJadeDragonStrike:Play("defensive")
-		else
-			warnJadeDragonStrike:Show()
-		end
+		self:BossTargetScanner(args.sourceGUID, "JadeDragonStrikeTarget", 0.1, 2)
 		timerJadeDragonStrikeCD:Start()
 	elseif spellId == 106856 then
 		specWarnDragonKick:Show()
@@ -79,16 +100,33 @@ end
 			specWarnJadeDragonKick:Play("runout")
 		end
 		specWarnJadeDragonKick:ScheduleVoice(1, "watchwave")
-	elseif spellId == 396907 then
+	elseif spellId == 396907 then --Огненное нефритовое дыхание
 		specWarnJadeBreath:Show()
 		specWarnJadeBreath:Play("breathsoon")
+		timerJadeBreathCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_REMOVED(args)
 	if args.spellId == 106797 then--Jade Essence removed, (Phase 3 trigger)
-		warnPhase3:Show()
 		timerJadeDragonStrikeCD:Cancel()
+	end
+end
+
+function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+	for i = 1, 5 do
+		local unitID = "boss"..i
+		local GUID = UnitGUID(unitID)
+		local cid = self:GetCIDFromGUID(GUID)
+		if cid == 56762 then -- Юй-лун
+			DBM:AddMsg("Murchal proshlyap")
+		--	DBM:Debug("Murchal proshlyap", 2)
+			warnPhase:Show(DBM_CORE_L.AUTO_ANNOUNCE_TEXTS.stage:format(3))
+			warnPhase:Play("pthree")
+			timerJadeDragonStrikeCD:Stop()
+			timerJadeBreathCD:Start(6)
+			timerJadeFireCD:Start(10)
+		end
 	end
 end
 
@@ -108,13 +146,10 @@ function mod:SPELL_PERIODIC_DAMAGE(_, _, _, _, destGUID, _, _, _, spellId, spell
 end
 mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
 
---[[
---12/18 19:35:56.323  UNIT_DIED,0000000000000000,nil,0x80000000,0x80000000,Creature-0-4218-960-7065-56732-00001FAC81,"Liu Flameheart",0xa48,0x0,0
---12/18 19:35:56.323  ENCOUNTER_END,1416,"Liu Flameheart",8,5,1,93650
-function mod:UNIT_DIED(args)
-	local cid = self:GetCIDFromGUID(args.destGUID)
-	if cid == 56762 then--Fight ends when Yu'lon dies.
-		DBM:EndCombat(self)
+function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, spellId)
+	if spellId == 107098 then --Нефритовый огонь
+		specWarnJadeFire:Show()
+		specWarnJadeFire:Play("watchstep")
+		timerJadeFireCD:Start()
 	end
 end
---]]
