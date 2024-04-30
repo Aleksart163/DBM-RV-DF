e311336193975305ca20966d8bf60fca14d34905
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2024-04-30 20:55:28 +0400

    up and fix

--- DBM-Party-WoD/SMBurialGrounds/Nhallish.lua
+++ DBM-Party-WoD/SMBurialGrounds/Nhallish.lua
@@ -12,7 +12,7 @@ mod.sendMainBossGUID = true
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 152801 153067 152792",
+	"SPELL_CAST_START 152801 153067 152792 153623 152962",
 	"SPELL_AURA_APPLIED 152979 153033",
 	"SPELL_AURA_REMOVED 152979 153033",
 	"SPELL_PERIODIC_DAMAGE 153070",
@@ -33,8 +33,10 @@ local specWarnVoidDevastation	= mod:NewSpecialWarningDodge(153067, nil, nil, nil
 local specWarnVoidDevastationM	= mod:NewSpecialWarningGTFO(153070, nil, nil, nil, 1, 8)
 local specWarnReturnedSoul		= mod:NewSpecialWarningYou(153033, nil, nil, nil, 1, 2) --Вернувшаяся душа
 
-local timerVoidVortexCD			= mod:NewCDTimer(77, 152801, nil, nil, nil, 2) --Водоворот Бездны
-local timerSoulShredCD			= mod:NewNextTimer(77, 152979, nil, nil, nil, 7) --Осколок души
+local timerVoidBlastCD			= mod:NewCDTimer(77, 152792, nil, nil, nil, 3, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.DEADLY_ICON) --Вспышка Бездны
+local timerPlanarShiftCD		= mod:NewCDTimer(77, 153623, nil, nil, nil, 3) --Сдвиг плоскости
+local timerVoidVortexCD			= mod:NewCDTimer(77, 152801, nil, nil, nil, 2, nil, DBM_COMMON_L.DEADLY_ICON) --Водоворот Бездны
+local timerSoulStealCD			= mod:NewNextTimer(75, 152962, nil, nil, nil, 7) --Кража души
 local timerSoulShred			= mod:NewBuffFadesTimer(20, 152979, nil, nil, nil, 7) --Осколок души
 local timerReturnedSoul			= mod:NewBuffFadesTimer(20, 153033, nil, nil, nil, 7) --Вернувшаяся душа
 local timerVoidDevastationCD	= mod:NewNextTimer(77, 153067, nil, nil, nil, 2, nil, DBM_COMMON_L.DEADLY_ICON) --Опустошение Бездны
@@ -58,8 +60,10 @@ function mod:VoidBlastTarget(targetname, uId)
 end
 
 function mod:OnCombatStart(delay)
-	timerVoidVortexCD:Start(22.7-delay)
-	timerSoulShredCD:Start(37-delay)
+	timerVoidBlastCD:Start(10.5-delay) --
+	timerPlanarShiftCD:Start(25.5-delay) --
+	timerVoidVortexCD:Start(27.5-delay) --
+	timerSoulStealCD:Start(37-delay)
 	timerVoidDevastationCD:Start(65.3-delay)
 end
 
@@ -75,6 +79,11 @@ function mod:SPELL_CAST_START(args)
 		timerVoidDevastationCD:Start()
 	elseif spellId == 152792 then --Вспышка Бездны
 		self:BossTargetScanner(args.sourceGUID, "VoidBlastTarget", 0.1, 2)
+		timerVoidBlastCD:Start()
+	elseif spellId == 152962 then --Кража души
+		timerSoulStealCD:Start()
+	elseif spellId == 153623 then --Сдвиг плоскости
+		timerPlanarShiftCD:Start()
 	end
 end
 
@@ -85,9 +94,6 @@ function mod:SPELL_AURA_APPLIED(args)
 			specWarnSoulShred:Show()
 			specWarnSoulShred:Play("killspirit")
 			timerSoulShred:Start()
-		end
-		if self:AntiSpam(2, 1) then
-			timerSoulShredCD:Start()
 		end
 	elseif spellId == 153033 then --Вернувшаяся душа
 		if args:IsPlayer() then
