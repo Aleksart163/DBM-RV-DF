3de6e857da1947acacc02ba547fbecfc03393059
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2024-06-28 04:55:33 +0400

    fix and up

--- DBM-Party-Dragonflight/HallsofInfusion/WatcherIrideus.lua
+++ DBM-Party-Dragonflight/HallsofInfusion/WatcherIrideus.lua
@@ -4,7 +4,7 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision("20240412075414")
 mod:SetCreatureID(189719)
 mod:SetEncounterID(2615)
-mod:SetHotfixNoticeRev(20230507000000)
+mod:SetHotfixNoticeRev(20240628070000)
 --mod:SetMinSyncRevision(20211203000000)
 --mod.respawnTime = 29
 mod.sendMainBossGUID = true
@@ -26,38 +26,57 @@ mod:RegisterEventsInCombat(
  or (source.type = "NPC" and source.firstSeen = timestamp) or (target.type = "NPC" and target.firstSeen = timestamp)
 --]]
 --Stage One: A Chance at Redemption
-mod:AddTimerLine(DBM:EJ_GetSectionInfo(25745))
-local warnPowerLoverload						= mod:NewTargetAnnounce(389179, 3)
+--mod:AddTimerLine(DBM:EJ_GetSectionInfo(25745))
+local warnPowerLoverload						= mod:NewTargetAnnounce(389179, 3) --Перегрузка
 
-local specWarnPowerOverload						= mod:NewSpecialWarningMoveAway(389179, nil, nil, nil, 1, 2)
-local specWarnSparkVolley						= mod:NewSpecialWarningDodge(384351, nil, nil, nil, 2, 2)
-local specWarnStaticSurge						= mod:NewSpecialWarningCount(384014, nil, nil, nil, 2, 2)
-local specWarnGTFO								= mod:NewSpecialWarningGTFO(389181, nil, nil, nil, 1, 8)
-local specWarnTitanticFist						= mod:NewSpecialWarningDodge(384524, nil, nil, nil, 1, 2)
+local specWarnPowerOverload						= mod:NewSpecialWarningMoveAway(389179, nil, nil, nil, 1, 2) --Перегрузка
+local specWarnSparkVolley						= mod:NewSpecialWarningDodge(384351, nil, nil, nil, 4, 2) --Череда разрядов
+local specWarnStaticSurge						= mod:NewSpecialWarningCount(384014, nil, nil, nil, 2, 2) --Статический выброс
+local specWarnGTFO								= mod:NewSpecialWarningGTFO(389181, nil, nil, nil, 1, 8) --Статическое поле
+local specWarnTitanticFist						= mod:NewSpecialWarningDodge(384524, nil, nil, nil, 2, 2) --Кулак титана
 
-local timerPowerOverloadCD						= mod:NewCDTimer(27.5, 389179, nil, nil, nil, 3, nil, DBM_COMMON_L.MAGIC_ICON)
-local timerSparkVolleyCD						= mod:NewCDTimer(31.6, 384351, nil, nil, nil, 3, nil, DBM_COMMON_L.MAGIC_ICON)
-local timerStaticSurgeCD						= mod:NewCDCountTimer(27.5, 384014, nil, nil, nil, 2)
-local timerTitanicFistCD						= mod:NewCDTimer(16.9, 384524, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
---Stage Two: Watcher's Last Stand
-mod:AddTimerLine(DBM:EJ_GetSectionInfo(25744))
-local warnAblativeBarrier						= mod:NewSpellAnnounce(383840, 2)
-local warnAblativeBarrierOver					= mod:NewEndAnnounce(383840, 1)
-local warnNullifyingPulse						= mod:NewCastAnnounce(389446, 4)
-local warnPurifyingBlast						= mod:NewTargetNoFilterAnnounce(389443, 3, nil, false)
+local timerPowerOverloadCD						= mod:NewCDTimer(60, 389179, nil, nil, nil, 3, nil, DBM_COMMON_L.MAGIC_ICON) --Перегрузка
+local timerSparkVolleyCD						= mod:NewCDTimer(60, 384351, nil, nil, nil, 3, nil, DBM_COMMON_L.MAGIC_ICON) --Череда разрядов
+local timerStaticSurgeCD						= mod:NewCDCountTimer(60, 384014, nil, nil, nil, 2) --Статический выброс
+local timerTitanicFistCD						= mod:NewCDTimer(60, 384524, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON) --Кулак титана
+--Perephase: Ochken of Murchal
+--mod:AddTimerLine(DBM:EJ_GetSectionInfo(25744))
+local warnAblativeBarrier						= mod:NewSpellAnnounce(383840, 2) --Абляционный барьер
+local warnAblativeBarrierOver					= mod:NewEndAnnounce(383840, 1) --Абляционный барьер
+local warnNullifyingPulse						= mod:NewCastAnnounce(389446, 4) --Нейтрализующая пульсация
+local warnPurifyingBlast						= mod:NewTargetNoFilterAnnounce(389443, 3, nil, false) --Очищающая вспышка
 
-local yellPowerOverload							= mod:NewYell(389179, nil, nil, nil, "YELL")
-local yellPowerOverloadFades					= mod:NewShortFadesYell(389179, nil, nil, nil, "YELL")
+local yellPowerOverload							= mod:NewYell(389179, nil, nil, nil, "YELL") --Перегрузка
+local yellPowerOverloadFades					= mod:NewShortFadesYell(389179, nil, nil, nil, "YELL") --Перегрузка
 
 mod.vb.surgeCount = 0
+mod.vb.titanicFistCount = 0
+mod.vb.ochkenProshlyapationsCount = 1
+
+local allProshlyapationsOfMurchal = {
+	[1] = {
+		--Кулак титана
+		[384524] = {6, 18, 22.1, 18.1, 19.4, 18.5, 18},
+		--Статический выброс
+		[384014] = {10.9, 28, 28, 28, 28.9, 60},
+	},
+	[2] = {
+		--Кулак титана
+		[384524] = {8.1, 18, 22.1, 18, 18, 19.9, 18, 18},
+		--Статический выброс
+		[384014] = {13, 28, 28, 28, 28.6, 60},
+	},
+}
 
 function mod:OnCombatStart(delay)
 	self.vb.surgeCount = 0
+	self.vb.titanicFistCount = 0
+	self.vb.ochkenProshlyapationsCount = 1
 	self:SetStage(1)
-	timerTitanicFistCD:Start(6-delay)
-	timerStaticSurgeCD:Start(10-delay, 1)
-	timerPowerOverloadCD:Start(23.4-delay)--20.6 (old?)
-	timerSparkVolleyCD:Start(29.1-delay)--37.4 (old?)
+	timerTitanicFistCD:Start(6-delay)--
+	timerStaticSurgeCD:Start(10.9-delay, 1)--
+	timerPowerOverloadCD:Start(26.1-delay)--
+	timerSparkVolleyCD:Start(29.9-delay)--
 end
 
 function mod:SPELL_CAST_START(args)
@@ -68,16 +87,23 @@ function mod:SPELL_CAST_START(args)
 		specWarnSparkVolley:Show()
 		specWarnSparkVolley:Play("watchstep")
 		timerSparkVolleyCD:Start()
-	elseif spellId == 384014 then
+	elseif spellId == 384014 then --Статический выброс
 		self.vb.surgeCount = self.vb.surgeCount + 1
 		specWarnStaticSurge:Show(self.vb.surgeCount)
 		specWarnStaticSurge:Play("aesoon")
-		timerStaticSurgeCD:Start(nil, self.vb.surgeCount+1)
-	elseif spellId == 384524 then
-		timerTitanicFistCD:Start()
+		local timer = self:GetFromTimersTable(allProshlyapationsOfMurchal, false, self.vb.ochkenProshlyapationsCount, spellId, self.vb.surgeCount+1)
+		if timer then
+			timerStaticSurgeCD:Start(timer, self.vb.surgeCount+1)
+		end
+	elseif spellId == 384524 then --Кулак титана
+		self.vb.titanicFistCount = self.vb.titanicFistCount + 1
 		if self:IsTanking("player", "boss1", nil, true) then
 			specWarnTitanticFist:Show()
 			specWarnTitanticFist:Play("shockwave")
+		end
+		local timer = self:GetFromTimersTable(allProshlyapationsOfMurchal, false, self.vb.ochkenProshlyapationsCount, spellId, self.vb.titanicFistCount+1)
+		if timer then
+			timerTitanicFistCD:Start(timer, self.vb.titanicFistCount+1)
 		end
 	elseif spellId == 389446 and self:AntiSpam(3, 1) then
 		warnNullifyingPulse:Show()
@@ -87,22 +113,20 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
 	if spellId == 389179 then
-		warnPowerLoverload:CombinedShow(0.3, args.destName)
 		if args:IsPlayer() then
 			specWarnPowerOverload:Show()
 			specWarnPowerOverload:Play("runout")
 			yellPowerOverload:Yell()
 			yellPowerOverloadFades:Countdown(spellId)
+		else
+			warnPowerLoverload:CombinedShow(0.3, args.destName)
 		end
-	elseif spellId == 383840 then
+	elseif spellId == 383840 then --Абляционный барьер
 		warnAblativeBarrier:Show()
-		if self:GetStage(1) then
-			self:SetStage(2)
-			timerPowerOverloadCD:Stop()
-			timerSparkVolleyCD:Stop()
-			timerStaticSurgeCD:Stop()
-			timerTitanicFistCD:Stop()
-		end
+		timerPowerOverloadCD:Stop()
+		timerSparkVolleyCD:Stop()
+		timerStaticSurgeCD:Stop()
+		timerTitanicFistCD:Stop()
 	elseif spellId == 389443 then
 		warnPurifyingBlast:CombinedShow(1, args.destName)
 	end
@@ -114,13 +138,18 @@ function mod:SPELL_AURA_REMOVED(args)
 		if args:IsPlayer() then
 			yellPowerOverloadFades:Cancel()
 		end
-	elseif spellId == 383840 then
+	elseif spellId == 383840 then --Абляционный барьер
+		self.vb.ochkenProshlyapationsCount = self.vb.ochkenProshlyapationsCount + 1
+		self.vb.surgeCount = 0
+		self.vb.titanicFistCount = 0
 		warnAblativeBarrierOver:Show()
-		self:SetStage(1)
-		timerTitanicFistCD:Start(6.1)
-		timerStaticSurgeCD:Start(10.7, self.vb.surgeCount+1)
-		timerPowerOverloadCD:Start(24.3)
-		timerSparkVolleyCD:Start(31.6)
+		if self:GetStage(1) then
+			self:SetStage(2)
+		end
+		timerTitanicFistCD:Start(8.1)--
+		timerStaticSurgeCD:Start(13, 1)--
+		timerPowerOverloadCD:Start(30)--
+		timerSparkVolleyCD:Start(31.6)--
 	end
 end
 
