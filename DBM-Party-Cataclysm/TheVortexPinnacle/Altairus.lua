ad29487d36f799d29c5dd2a9e24eb4456b9d5714
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2024-08-10 03:49:45 +0400

    fix and up

--- DBM-Party-Cataclysm/TheVortexPinnacle/Altairus.lua
+++ DBM-Party-Cataclysm/TheVortexPinnacle/Altairus.lua
@@ -7,7 +7,6 @@ mod.upgradedMPlus = true
 mod:SetRevision("20240426175442")
 mod:SetCreatureID(43873)
 mod:SetEncounterID(1041)
-mod:SetUsedIcons(8)
 mod:SetHotfixNoticeRev(20230527000000)
 --mod:SetMinSyncRevision(20230226000000)
 mod.sendMainBossGUID = true
@@ -29,43 +28,26 @@ ability.id = 88308 and type = "begincast"
  or (ability.id = 413295 or ability.id = 181089) and type = "cast"
  or type = "dungeonencounterstart" or type = "dungeonencounterend"
 --]]
---local warnBreath			= mod:NewTargetNoFilterAnnounce(88308, 2)
-local warnCalltheWind		= mod:NewCountAnnounce(88276, 2)
-local warnUpwind			= mod:NewSpellAnnounce(88282, 1)
+
+local warnCalltheWind		= mod:NewCountAnnounce(88276, 2) --Призыв ветра
+local warnUpwind			= mod:NewSpellAnnounce(88282, 1) --Наветренная сторона
 
 --local specWarnBreath		= mod:NewSpecialWarningYou(88308, "-Tank", nil, 2, 1, 2)
-local specWarnBreath		= mod:NewSpecialWarningDodgeCount(88308, nil, nil, nil, 2, 2)
-local specWarnDownburst		= mod:NewSpecialWarningMoveTo(413295, nil, nil, nil, 2, 14, 4)
-local specWarnDownwind		= mod:NewSpecialWarningSpell(88286, nil, nil, nil, 1, 14)
-local specWarnGTFO			= mod:NewSpecialWarningGTFO(413275, nil, nil, nil, 1, 8)
+local specWarnBreath		= mod:NewSpecialWarningDodge(88308, nil, 18357, nil, 2, 2) --Студеное дыхание (Дыхание)
+local specWarnDownburst		= mod:NewSpecialWarningMoveTo(413295, nil, nil, nil, 2, 14, 4) --Нисходящий порыв
+local specWarnDownwind		= mod:NewSpecialWarningSpell(88286, nil, nil, nil, 1, 14) --Подветренная сторона Альтаирия
+local specWarnGTFO			= mod:NewSpecialWarningGTFO(413275, nil, nil, nil, 1, 8) --Холодный фронт
 
-local timerCalltheWindCD	= mod:NewCDCountTimer(20.6, 88276, nil, nil, nil, 6)
-local timerBreathCD			= mod:NewCDCountTimer(13.4, 88308, nil, nil, nil, 3)--May be 10.5 pre nerf for cata classic
-local timerDownburstCD		= mod:NewCDCountTimer(35.1, 413295, nil, nil, nil, 3, nil, DBM_COMMON_L.MYTHIC_ICON)--35.1-44
-
---mod:AddSetIconOption("BreathIcon", 88308, true, false, {8})
+local timerCalltheWindCD	= mod:NewCDCountTimer(20.6, 88276, nil, nil, nil, 6) --Призыв ветра
+local timerBreathCD			= mod:NewCDCountTimer(13.4, 88308, 18357, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON) --Студеное дыхание (Дыхание) May be 10.5 pre nerf for cata classic
+local timerDownburstCD		= mod:NewCDCountTimer(35.1, 413295, nil, nil, nil, 7) --Нисходящий порыв 35.1-44
 
 mod.vb.activeWind = "none"
 mod.vb.windCount = 0
 mod.vb.burstCount = 0
 mod.vb.breathCount = 0
-local tornado = DBM:GetSpellName(86133)
 
---[[
-function mod:BreathTarget()
-	local targetname = self:GetBossTarget(43873)
-	if not targetname then return end
-	if self.Options.BreathIcon then
-		self:SetIcon(targetname, 8, 4)
-	end
-	if targetname == UnitName("player") then--Tank doesn't care about this so if your current spec is tank ignore this warning.
-		specWarnBreath:Show()
-		specWarnBreath:Play("targetyou")
-	else
-		warnBreath:Show(targetname)
-	end
-end
---]]
+local tornado = DBM:GetSpellName(86133)
 
 local function updateAllTimers(self, ICD)
 	DBM:Debug("updateAllTimers running", 3)
@@ -89,56 +71,64 @@ local function updateAllTimers(self, ICD)
 	end
 end
 
+local allProshlyapationsOfMurchal = {
+	--Дыхание
+	[88308] = {13.4, 21, 20.9, 21.1, 21, 21, 20, 21.1},
+}
+--каждые 19.9 Призыв ветра
 function mod:OnCombatStart(delay)
 	self.vb.activeWind = "none"
 	self.vb.windCount = 0
 	self.vb.burstCount = 0
 	self.vb.breathCount = 0
-	timerCalltheWindCD:Start(5-delay, 1)
-	if self:IsMythicPlus() then
-		timerBreathCD:Start(self:IsMythicPlus() and 12.1-delay, 1)
-		timerDownburstCD:Start(20.5-delay, 1)
+	if self:IsMythic() then
+		timerCalltheWindCD:Start(5.9-delay, 1) --
+		timerBreathCD:Start(13.4-delay, 1) --
+		timerDownburstCD:Start(21.5-delay, 1) --
 	else
 		--TODO, recheck on non mythic plus
+		timerCalltheWindCD:Start(5-delay, 1)
 		timerBreathCD:Start(10.7-delay, 1)
 	end
 end
 
 function mod:SPELL_CAST_START(args)
-	if args.spellId == 88308 then
---		self:ScheduleMethod(0.2, "BreathTarget")
+	local spellId = args.spellId
+	if spellId == 88308 then --Студеное дыхание
 		self.vb.breathCount = self.vb.breathCount + 1
-		specWarnBreath:Show(self.vb.breathCount)
+		specWarnBreath:Show()
 		specWarnBreath:Play("breathsoon")
-		timerBreathCD:Start(self:IsMythicPlus() and 21.8 or 13.4, self.vb.breathCount+1)
+		timerBreathCD:Start(self:IsMythic() and 21.8 or 13.4, self.vb.breathCount+1)
 		updateAllTimers(self, 6)
 	end
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 413295 then
+	local spellId = args.spellId
+	if spellId == 413295 then
 		self.vb.burstCount = self.vb.burstCount + 1
 		specWarnDownburst:Show(tornado)
 		specWarnDownburst:Play("getknockedup")
 		timerDownburstCD:Start(nil, self.vb.burstCount+1)
 		--updateAllTimers(self, 1.2)--accurate, but not really worth triggering
-	elseif args.spellId == 181089 then--Encounter Event
+	elseif spellId == 181089 then--Encounter Event
 		self.vb.windCount = self.vb.windCount + 1
 		warnCalltheWind:Show(self.vb.windCount)
-		timerCalltheWindCD:Start(self:IsMythicPlus() and 15.4 or 20.6, self.vb.windCount+1)
+		timerCalltheWindCD:Start(self:IsMythic() and 15.4 or 20.6, self.vb.windCount+1)
 		--updateAllTimers(self, 1.2)--accurate, but not really worth triggering
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 88282 and args:IsPlayer() and self.vb.activeWindactiveWind ~= "up" then
+	local spellId = args.spellId
+	if spellId == 88282 and args:IsPlayer() and self.vb.activeWind ~= "up" then
 		warnUpwind:Show()
-		self.vb.activeWindactiveWind = "up"
-	elseif args.spellId == 88286 and args:IsPlayer() and self.vb.activeWindactiveWind ~= "down" then
+		self.vb.activeWind = "up"
+	elseif spellId == 88286 and args:IsPlayer() and self.vb.activeWind ~= "down" then
 		specWarnDownwind:Show()
 		specWarnDownwind:Play("getupwind")
-		self.vb.activeWindactiveWind = "down"
-	elseif args.spellId == 413275 and args:IsPlayer() and self:AntiSpam(2.5, 1) then
+		self.vb.activeWind = "down"
+	elseif spellId == 413275 and args:IsPlayer() and self:AntiSpam(2.5, 1) then
 		specWarnGTFO:Show(args.spellName)
 		specWarnGTFO:Play("watchfeet")
 	end
