5dd8c018981f9d1ef9ee58119166b0b71aeacece
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2024-08-18 00:54:20 +0400

    fix

--- DBM-Affixes/MPlusAffixes.lua
+++ DBM-Affixes/MPlusAffixes.lua
@@ -49,8 +49,8 @@ local timerQuake							= mod:NewCastTimer(2.5, 240447, nil, nil, nil, 2, nil, DB
 local timerNecroticWound					= mod:NewBuffActiveTimer(9, 209858, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON..DBM_COMMON_L.HEALER_ICON) --Некротическая язва
 local timerBurst							= mod:NewBuffActiveTimer(4, 240443, nil, nil, nil, 3, nil, DBM_COMMON_L.MYTHIC_ICON..DBM_COMMON_L.DEADLY_ICON) --Взрыв
 --
-local timerQuakingCD						= mod:NewNextTimer(20, 240447, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON) --Землетрясение
-local timerEntangledCD						= mod:NewCDTimer(30, 408556, 311634, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON, nil, nil, nil, nil, nil, nil, true) --Запутывание (Гнев деревьев)
+local timerQuakingCD						= mod:NewCDTimer(20, 240447, nil, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON) --Землетрясение
+local timerEntangledCD						= mod:NewNextTimer(30, 408556, 311634, nil, nil, 3, nil, DBM_COMMON_L.DEADLY_ICON, nil, nil, nil, nil, nil, nil, true) --Запутывание (Гнев деревьев)
 local timerAfflictedCD						= mod:NewCDTimer(30, 409492, nil, nil, nil, 5, 2, DBM_COMMON_L.HEALER_ICON, nil, mod:IsHealer() and 3 or nil, 3)--Крик изнемогающей души Timer is still on for all, cause knowing when they spawn still informs decisions like running ahead or pulling
 local timerIncorporealCD					= mod:NewCDTimer(45, 408801, 173254, nil, nil, 5, nil, nil, nil, 3, 3) --Бесплотность (Призыв духов) 173254
 
