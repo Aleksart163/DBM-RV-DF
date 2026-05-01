0ed8ef70a8fb2d9c742f3755cf21e37663b4f664
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2026-05-01 04:58:44 +0400

    up

--- DBM-Core/localization.en.lua
+++ DBM-Core/localization.en.lua
@@ -3,7 +3,8 @@ local L = {}
 
 DBM_CORE_L = L
 
-L.DBM_RV_FORUMS_MESSAGE					= "Found a bug or wrong timer? Do you think that some module needs an additional warning, timer, feature or localization? \nAny questions can be addressed to discord |h|cff3588ffAleksart163|r, /w |h|cff3588ffКуплиняш|r or leave requests at |h|cff3588ffhttps://github.com/Aleksart163/DBM-RV-DF/pulls|r\n|h|cffa0000fATTENTION!|r Starting from season 3, the support for this addon is temporarily discontinued. You can still support the addon at |h|cff3588ffhttps://www.donationalerts.com/r/aleksart163|r"
+--L.DBM_RV_FORUMS_MESSAGE					= "Found a bug or wrong timer? Do you think that some module needs an additional warning, timer, feature or localization? \nAny questions can be addressed to discord |h|cff3588ffAleksart163|r, /w |h|cff3588ffКуплиняш|r or leave requests at |h|cff3588ffhttps://github.com/Aleksart163/DBM-RV-DF/pulls|r\n|h|cffa0000fATTENTION!|r Starting from season 3, the support for this addon is temporarily discontinued. You can still support the addon at |h|cff3588ffhttps://www.donationalerts.com/r/aleksart163|r"
+L.DBM_RV_FORUMS_MESSAGE					= "Found a bug or wrong timer? Do you think that some module needs an additional warning, timer, feature or localization? \nAny questions can be addressed to discord |h|cff3588ffAleksart163|r, /w |h|cff3588ffКуплиняш|r or leave requests at |h|cff3588ffhttps://github.com/Aleksart163/DBM-RV-DF/pulls|r"
 
 L.DEADLY_BOSS_MODS						= "Dungeon Boss Master" -- NO TRANSLATE
 L.DBM									= "DBM RV" -- NO TRANSLATE
@@ -11,7 +12,7 @@ L.DBM_RV_PROSHLYAPATION_OF_MURCHAL_URL	= "https://github.com/Aleksart163/DBM-RV-
 
 local dateTable = date("*t")
 if dateTable.day and dateTable.month and dateTable.day == 1 and dateTable.month == 4 then
-	L.DEADLY_BOSS_MODS					= "Murchal Ochken Proshlyapation"
+	L.DEADLY_BOSS_MODS					= "Murchal Ochken Proshlyap"
 	L.DBM								= "MOP"
 end
 
