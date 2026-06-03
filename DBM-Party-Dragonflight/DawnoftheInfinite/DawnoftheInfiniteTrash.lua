4d51b1e903fb8e7a9bc70c4967c37dc4cce5420f
Arekusandoru <82957716+Aleksart163@users.noreply.github.com>
2026-06-03 15:40:26 +0400

    fix

--- DBM-Party-Dragonflight/DawnoftheInfinite/DawnoftheInfiniteTrash.lua
+++ DBM-Party-Dragonflight/DawnoftheInfinite/DawnoftheInfiniteTrash.lua
@@ -495,25 +495,27 @@ function mod:GOSSIP_SHOW()
 end
 
 function mod:CHAT_MSG_MONSTER_YELL(msg)
-	if (msg == L.MurchalProshlyapRP2 or msg:find(L.MurchalProshlyapRP2)) then
+	if (msg == L.MurchalProshlyapRP1 or msg:find(L.MurchalProshlyapRP1)) then
+		self:SendSync("RP1")
+	elseif (msg == L.MurchalProshlyapRP2 or msg:find(L.MurchalProshlyapRP2)) then
 		self:SendSync("RP2")
 	elseif (msg == L.MurchalProshlyapRP3 or msg:find(L.MurchalProshlyapRP3)) then
 		self:SendSync("RP3")
 	end
 end
 
-function mod:CHAT_MSG_MONSTER_SAY(msg)
+--[[function mod:CHAT_MSG_MONSTER_SAY(msg)
 	if (msg == L.MurchalProshlyapRP1 or msg:find(L.MurchalProshlyapRP1)) then
 		self:SendSync("RP1")
 	end
-end
+end]]
 
 function mod:OnSync(msg)
 	if msg == "RP1" and self:AntiSpam(10, 2) then --Таймер пула после Гнили
-		timerRP:Start(85.5)
+		timerRP:Start(74)
 	elseif msg == "RP2" and self:AntiSpam(10, 2) then --Таймер пула Иридикрона
 		timerRP:Start(29)
 	elseif msg == "RP3" and self:AntiSpam(10, 2) then --Таймер пула после Гнили 2
-		timerRP:Start(81)
+		timerRP:Start(88)
 	end
 end
